2033c72cccda63fb4d64e3e56141389664137a68
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2025-03-20 19:19:38 +0530

    Sample project to simulate the issue https://github.com/dotnet/maui/issues/28537

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# maui-issue-28537
+## maui-issue-28537
+
 Sample project to simulate the issue https://github.com/dotnet/maui/issues/28537
---
+++ src/NET_9/MauiApp3/MauiApp3.MauiLib/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using MauiApp3.Data;
+using Microsoft.Extensions.Logging;
+
+namespace MauiApp3
+{
+    public static partial class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder.UseMauiApp<App>()
+                   .ConfigureFonts(fonts =>
+                   {
+                       fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                       fonts.AddFont("OpenSans-SemiBold.ttf", "OpenSansSemiBold");
+                   });
+
+            builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddSingleton(AppInfo.Current);
+            builder.Services.AddSingleton<WeatherForecastService>();
+
+#if DEBUG
+            // Caution: Recommended to enable Developer Tools only for development!!!
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
